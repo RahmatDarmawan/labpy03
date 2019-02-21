044c0ba34dfc60c228e87d97e6380055863a6931
Rahmat darmawan <46924922+RahmatDarmawan@users.noreply.github.com>
2019-02-21 15:50:28 +0700

    Add files via upload

---
+++ latihan2.py
@@ -0,0 +1,11 @@
+print("******* Latihan 2 *******")
+print("Menampilkan bilangan, berhenti ketika bilangan 0, dan menampilkan bilangan terbesar")
+
+b = 0
+while True:
+    angka = int(input("Masukkan Bilangan : "))
+    b = angka
+    if b == 0:
+        break
+print("******* Selesai *******")
+
